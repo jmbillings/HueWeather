de6b7f11ed12f2aa14334d60da3fec6af23fee9f
jmbillings <james@puffle.co.uk>
2015-12-01 21:59:32 +0000

    Base solution

---
+++ HueWeather/HueWeather/StartupTask.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net.Http;
+using Windows.ApplicationModel.Background;
+using Windows.System.Threading;
+
+// The Background Application template is documented at http://go.microsoft.com/fwlink/?LinkID=533884&clcid=0x409
+
+namespace HueWeather
+{
+    public sealed class StartupTask : IBackgroundTask
+    {
+        public void Run(IBackgroundTaskInstance taskInstance)
+        {
+            taskInstance.GetDeferral();
+            Webserver server = new Webserver();
+            ThreadPool.RunAsync(workItem =>
+            {
+                server.Start();
+            });
+        }
+    }
+}
